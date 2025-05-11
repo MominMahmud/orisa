3dc7ae82e53cceca617a05807aca285f4398e310
Momin Mahmud <mominmahmudjalib@gmail.com>
2025-05-11 00:37:17 -0400

    fixed some small

--- orisa/src/components/shared/Section/FeaturesSection.tsx
+++ orisa/src/components/shared/Section/FeaturesSection.tsx
@@ -2,19 +2,19 @@ import styles from './FeaturesSection.module.scss';
 
 const features = [
   {
-    icon: 'public/data_color.png',
+    icon: '/data_color.png',
     text: 'Rapid data aggregation and processing',
   },
   {
-    icon: 'public/model_color.png',
+    icon: '/model_color.png',
     text: 'State of the art deep learning models streamlining analysis',
   },
   {
-    icon: 'public/nlp_color.png',
+    icon: '/nlp_color.png',
     text: 'Intuitive natural language interface',
   },
   {
-    icon: 'public/privacy_color.png',
+    icon: '/privacy_color.png',
     text: 'Enterprise-ready with data privacy and governance controls',
   },
 ];
